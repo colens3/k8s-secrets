069e5f4088c2e443597874801cc006ed043c4975
Stojan Nedic <colens3@gmail.com>
2021-02-21 14:39:47 +0100

    Add secrets_update script which uses k8s py client

---
+++ secrets_update.py
@@ -0,0 +1,47 @@
+import base64
+from datetime import datetime
+from kubernetes import client, config
+
+def main():
+    """Method which will update secret and update annotation through k8s client lib
+    """
+    secret = "updated_user2"
+
+    # Generate base64 needed for secret update
+    secret_bytes = secret.encode('ascii')
+    base64_bytes = base64.b64encode(secret_bytes)
+    secret_encoded = base64_bytes.decode('ascii')
+
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config()
+    client_v1 = client.CoreV1Api()
+    
+    # Update secret
+    ret = client_v1.patch_namespaced_secret(
+        "test-secret",
+        "default",
+        {
+            "data": {
+                "username": secret_encoded
+            }
+        }
+    )
+    # Update annotation on specific pod
+    # it's also possible implement update on all namespaced pods)
+    ret = client_v1.patch_namespaced_pod(
+        "k8s-python-pod",
+        "default",
+        {
+            "metadata": {
+                "annotations": {
+                    "secret_updated_at": datetime.now()
+                }
+            }
+        }
+    )
+
+    # ret = client_v1.list_namespaced_secret("default")
+
+
+if __name__ == "__main__":
+    main()
